acc6e04b194f01d681e3bcb1ca3ab69c1f9937fc
Miccah Castorina <m.castorina93@gmail.com>
2022-02-11 11:55:21 -0600

    Switch to termloop and build a simple level

--- main.go
+++ main.go
@@ -1,96 +1,89 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	tl "github.com/JoelOtter/termloop"
 )
 
-var (
-	teaOptions = []tea.ProgramOption{tea.WithAltScreen(), tea.WithOutput(os.Stderr)}
-	black      = "#231f20"
-	green      = "#5aba47"
-	white      = "#ffffff"
-)
-
-type model struct {
-	cursor int
-	// color grid
-	// TODO: more data than just color
-	grid [][]string
+// Player ...
+type Player struct {
+	*tl.Entity
+	prevX int
+	prevY int
 }
 
-func (m *model) Init() tea.Cmd {
-	return nil
-}
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			m.cursor++
+// Tick ...
+func (player *Player) Tick(event tl.Event) {
+	if event.Type == tl.EventKey { // Is it a keyboard event?
+		player.prevX, player.prevY = player.Position()
+		switch event.Key { // If so, switch on the pressed key.
+		case tl.KeyArrowRight:
+			player.SetPosition(player.prevX+1, player.prevY)
+		case tl.KeyArrowLeft:
+			player.SetPosition(player.prevX-1, player.prevY)
+		case tl.KeyArrowUp:
+			player.SetPosition(player.prevX, player.prevY-1)
+		case tl.KeyArrowDown:
+			player.SetPosition(player.prevX, player.prevY+1)
 		}
 	}
-	return m, nil
 }
 
-func (m *model) View() string {
-	var image string
-
-	for _, row := range m.grid {
-		for _, color := range row {
-			style := lipgloss.NewStyle().Background(lipgloss.Color(color)).Width(2).Height(1)
-			image += style.Render("")
-		}
-		image += "\n"
+// Collide ...
+func (player *Player) Collide(collision tl.Physical) {
+	// Check if it's a Rectangle we're colliding with
+	if _, ok := collision.(*tl.Rectangle); ok {
+		player.SetPosition(player.prevX, player.prevY)
 	}
-
-	return image
 }
 
 func main() {
-	// flag.Parse()
-	runCli()
-}
-
-func runCli() {
-	model := &model{
-		grid: [][]string{
-			{white, white, white, white, white, white, white, white, white, white, white, white, white, white},
-			{white, black, black, black, black, black, black, black, black, black, black, black, black, white},
-			{white, black, white, white, white, white, white, white, white, white, white, white, black, white},
-			{white, black, white, black, black, black, black, black, black, black, black, white, black, white},
-			{white, black, white, green, black, green, black, green, black, black, black, white, black, white},
-			{white, black, white, black, black, black, black, black, black, black, black, white, black, white},
-			{white, black, white, black, green, green, black, green, green, black, black, white, black, white},
-			{white, black, white, black, black, black, black, black, black, black, black, white, black, white},
-			{white, black, white, black, black, black, black, black, black, black, black, white, black, white},
-			{white, black, white, white, white, white, white, white, white, white, white, white, black, white},
-			{white, black, black, black, black, black, black, black, black, black, black, black, black, white},
-			{white, white, white, white, white, black, black, black, black, white, white, white, white, white},
-			{white, white, black, black, black, black, black, black, black, black, black, black, white, white},
-			{white, black, black, black, white, black, white, black, white, black, white, black, black, white},
-			{white, black, black, white, black, white, black, white, black, white, black, black, black, white},
-			{white, black, black, black, black, black, black, black, black, black, black, black, black, white},
-			{white, white, white, white, white, white, white, white, white, white, white, white, white, white},
-		},
+	g := tl.NewGame()
+	g.Screen().SetFps(30)
+	level := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorBlack,
+		Fg: tl.ColorBlack,
+	})
+	type rect struct {
+		x     int
+		y     int
+		w     int
+		h     int
+		color tl.Attr
 	}
-
-	program := tea.NewProgram(model, teaOptions...)
-
-	exitCode := 0
-	if err := program.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "clidle: %s\n", err)
-		exitCode = 1
+	for _, rect := range []rect{
+		{10, 10, 14, 17, tl.ColorWhite},
+		{11, 11, 12, 10, tl.ColorBlack},
+		{12, 12, 10, 8, tl.ColorWhite},
+		{13, 13, 8, 6, tl.ColorBlack},
+		{13, 14, 1, 1, tl.ColorGreen},
+		{15, 14, 1, 1, tl.ColorGreen},
+		{17, 14, 1, 1, tl.ColorGreen},
+		{14, 16, 2, 1, tl.ColorGreen},
+		{17, 16, 2, 1, tl.ColorGreen},
+		{15, 21, 4, 1, tl.ColorBlack},
+		{11, 22, 12, 4, tl.ColorBlack},
+		{11, 22, 1, 1, tl.ColorWhite},
+		{22, 22, 1, 1, tl.ColorWhite},
+		{14, 23, 1, 1, tl.ColorWhite},
+		{16, 23, 1, 1, tl.ColorWhite},
+		{18, 23, 1, 1, tl.ColorWhite},
+		{20, 23, 1, 1, tl.ColorWhite},
+		{13, 24, 1, 1, tl.ColorWhite},
+		{15, 24, 1, 1, tl.ColorWhite},
+		{17, 24, 1, 1, tl.ColorWhite},
+		{19, 24, 1, 1, tl.ColorWhite},
+	} {
+		level.AddEntity(tl.NewRectangle(
+			rect.x*2,
+			rect.y,
+			rect.w*2,
+			rect.h,
+			rect.color,
+		))
 	}
-	os.Exit(exitCode)
+	player := Player{tl.NewEntity(1, 1, 2, 1), 0, 0}
+	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	level.AddEntity(&player)
+	g.Screen().SetLevel(level)
+	g.Start()
 }
